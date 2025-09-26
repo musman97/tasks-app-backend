19527795e99aaf32ead0580bb67d47c238d565aa
Muhammad Usman <zahoor.usman.m@gmail.com>
2025-09-26 11:08:06 +0500

    feat: Add the ability to set status code of res

--- src/common/dto/response.dto.ts
+++ src/common/dto/response.dto.ts
@@ -1,12 +1,15 @@
+import { HttpStatus } from '@nestjs/common';
 import type { PaginatedResponseMeta } from '../interfaces';
 
 export class ResponseDto<D = undefined> {
   data?: D | null;
   message?: string;
+  statusCode?: HttpStatus;
 
-  constructor(data?: D, message?: string) {
+  constructor(statusCode = HttpStatus.OK, data?: D, message?: string) {
     this.data = data;
     this.message = message;
+    this.statusCode = statusCode;
   }
 }
 
@@ -14,10 +17,12 @@ export class PaginatedResponseDto<D> {
   data: D | null;
   message?: string | undefined;
   meta: PaginatedResponseMeta;
+  statusCode?: HttpStatus;
 
   constructor(data: D, meta: PaginatedResponseMeta, message?: string) {
     this.data = data;
     this.meta = meta;
     this.message = message;
+    this.statusCode = HttpStatus.OK;
   }
 }
--- src/common/interceptors/global.response.transform.interceptor.ts
+++ src/common/interceptors/global.response.transform.interceptor.ts
@@ -1,6 +1,12 @@
-import { CallHandler, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Response as EResponse } from 'express';
 import { PaginatedResponseDto, ResponseDto } from '../dto';
 import { PaginatedResponse, Response } from '../interfaces';
 
@@ -13,13 +19,20 @@ export class GlobalResponseTransformInterceptor<D>
     >
 {
   intercept(
-    _,
+    ctx: ExecutionContext,
     next: CallHandler<ResponseDto<D> | PaginatedResponseDto<D>>,
   ):
     | Observable<Response<D> | PaginatedResponse<D>>
     | Promise<Observable<Response<D> | PaginatedResponse<D>>> {
     return next.handle().pipe(
       map((responseData) => {
+        if (responseData.statusCode) {
+          ctx
+            .switchToHttp()
+            .getResponse<EResponse>()
+            .status(responseData.statusCode);
+        }
+
         if (responseData.data || responseData.message) {
           return { success: true, ...responseData };
         }
